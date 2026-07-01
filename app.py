685afd0ff0f61c4685531449ec54485503eea64f
IanRezende77 <12500763@aluno.cotemig.com.br>
2026-07-01 12:52:18 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,53 +1,35 @@
-# Cenário: C - Figurinhas
-# Aluno: Ian Erfurt Rezende
+import os
 
-from flask import Flask, redirect, url_for
+from flask import Flask
 
-from controllers import figurinhas_bp
-from models import Colecionador, Figurinha, db
+from controllers import api_v1_bp, dashboard_bp, figurinhas_bp
+from dados_iniciais import popular_dados
+from models import db
 
 
 def criar_app():
-    app = Flask(__name__, template_folder="views/templates")
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///aula11_figurinhas.db"
+    app = Flask(
+        __name__,
+        template_folder="views/templates",
+        static_folder="views/static",
+    )
+
+    pasta = os.path.abspath(os.path.dirname(__file__))
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+        pasta, "figurinhas.db"
+    )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
+    app.register_blueprint(dashboard_bp)
     app.register_blueprint(figurinhas_bp)
-
-    @app.route("/")
-    def home():
-        return redirect(url_for("figurinhas.index"))
+    app.register_blueprint(api_v1_bp)
 
     with app.app_context():
         db.create_all()
-        popular_banco_se_vazio()
+        popular_dados()
 
     return app
-
-
-def popular_banco_se_vazio():
-    if Colecionador.query.first() is None:
-        db.session.add_all(
-            [
-                Colecionador(apelido="Joaozinho", cidade="Belo Horizonte"),
-                Colecionador(apelido="Maria10", cidade="Contagem"),
-                Colecionador(apelido="CraqueBH", cidade="Betim"),
-            ]
-        )
-
-    if Figurinha.query.first() is None:
-        db.session.add_all(
-            [
-                Figurinha(numero=10, nome_jogador="Neymar", time="Brasil"),
-                Figurinha(numero=7, nome_jogador="Vini Jr", time="Brasil"),
-                Figurinha(numero=9, nome_jogador="Richarlison", time="Brasil"),
-                Figurinha(numero=30, nome_jogador="Messi", time="Argentina"),
-                Figurinha(numero=11, nome_jogador="Mbappe", time="Franca"),
-            ]
-        )
-
-    db.session.commit()
 
 
 app = criar_app()
